e4b522121edd830926b58b216d54742aa8ddee5e
Soham Patel <soham171203@gmail.com>
2024-08-03 18:14:13 +0530

    add
    add

---
+++ app.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img
+                    className="logo"
+                    src="http://www.psdtemplatedesign.com/wp-content/uploads/edd/2017/12/logo-design.jpg"
+                    alt="Logo"
+                />
+            </div>
+        </div>
+
+        // <div className="nav-item">
+        //     <ul>
+        //         <li>Home</li>
+        //         <li>Contact Us</li>
+        //         <li>About Us</li>
+        //         <li>Card</li>
+        //     </ul>
+        // </div>
+    );
+};
+// const card= () => {
+// return 
+// }
+
+const AppLayout = () => {
+    return <Header />;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
